92d150ca2a9255920f43cd1842af8a23cfee7383
Utkarsh Singh <dawno@users.noreply.github.com>
2016-07-09 23:47:11 +0530

    Boolean value and created at added in the php file

--- sendingmessage.php
+++ sendingmessage.php
@@ -9,20 +9,21 @@ $sender_name = $_POST['sender_name'];
 $message = $_POST['message'];
 $receiver_name = $_POST['receiver_name'];
 //$x= (String)date(DATE_RFC2822);
-$query = mysqli_query($con, "INSERT INTO chatdata (sender_name, message, receiver_name) VALUES ('$sender_name', '$message', '$receiver_name')");
+$query = mysqli_query($con, "INSERT INTO chatdata (sender_name, message, receiver_name, created_at) VALUES ('$sender_name', '$message', '$receiver_name', NOW())");
 
 $bool_query1 = mysqli_query($con, "UPDATE chatdata SET bool = '1' WHERE sender_name = ('$sender_name') ");
 
 $bool_query2 = mysqli_query($con, "UPDATE chatdata SET bool = '0' WHERE sender_name = ('$receiver_name')");
 
-$query_sender = mysqli_query($con, "SELECT * FROM chatdata WHERE (sender_name = '$sender_name' OR sender_name = '$receiver_name') AND (receiver_name = '$receiver_name' OR receiver_name = '$sender_name')" );
+$query_sender = mysqli_query($con, "SELECT * FROM chatdata WHERE (sender_name = '$sender_name' OR sender_name = '$receiver_name') AND (receiver_name = '$receiver_name' OR receiver_name = '$sender_name') ORDER BY created_at ASC" );
 
 $response = array();
 
-while($row = mysqli_fetch_array($query_sender)){
+while($row = mysqli_fetch_array($query_sender)){ 
 array_push($response,
 array('message'=>$row[1],
-'bool' => $row[4]
+'bool' => $row[4], 
+'created_at' => $row[3]
 ));
 }
 
@@ -33,6 +34,7 @@ else
  $response["error"] = TRUE;
  $response["error_msg"] = "Unknown error occurred in registration!";
 echo json_encode($response);
+
 }
 ?>
 
